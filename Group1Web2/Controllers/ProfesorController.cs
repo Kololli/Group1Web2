5ad056d78836146f82b18e0cf50295437d465485
Kololli <ledionkololli07@gmail.com>
2022-11-15 06:40:58 +0100

    Add project files.

---
+++ Group1Web2/Controllers/ProfesorController.cs
@@ -0,0 +1,27 @@
+﻿using Group1Web2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group1Web2.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        public IActionResult Index()
+        {
+            Profesori prof1 = new Profesori(emri:"Leron", mbiemri:"Berisha");
+            Profesori prof2 = new Profesori(emri: "Ledion", mbiemri: "Kololli");
+            Profesori prof3 = new Profesori(emri: "Hasan", mbiemri: "Hasani");
+            List<Profesori> profesoret = new List<Profesori>();
+                profesoret.Add(prof1);
+                profesoret.Add(prof2);
+                profesoret.Add(prof3);
+            return View(profesoret);
+
+        }
+
+        public IActionResult MesoHtml()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Group1Web2/Models/Profesori.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.FileProviders.Embedded;
+
+namespace Group1Web2.Models
+{
+    public class Profesori
+    {
+        public Profesori(string emri,string mbiemri)
+        {
+            Emri= emri;
+            Mbiemri= mbiemri;
+
+        }
+        public String Emri { get; set; }
+        public String Mbiemri { get; set; }
+    }
+}
